1f3c90fa33f6764cd16a18278c7c1af397d44bce
Tim Kay <tim.kay@internode.on.net>
2023-08-29 10:17:38 +1000

    Change for wait

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -29,15 +29,23 @@ Then (/^I enter in my (.*), (.*) and (.*)$/, async (accountname, bsb, accountnum
 
 Then (/^I confirm the payment has been processed with the correct outcome (.*)$/, async (paymentoutcome) => {
     await browser.saveScreenshot('./screenshots/screenshotpass.png');
-    await PaymentProcessingPage.paymentResult.waitForDisplayed({timeout: 15000});
-    await browser.saveScreenshot('./screenshots/screenshotpassafter15secs.png');
+    await PaymentProcessingPage.paymentResult.waitUntil(async function () {
+        return (await this.getText()) === paymentoutcome
+    },{
+        timeout: 120000
+    });
+    await browser.saveScreenshot('./screenshots/screenshotpassed.png');
     await expect(PaymentProcessingPage.paymentResult).toBeExisting();
     await expect(PaymentProcessingPage.paymentResult).toHaveTextContaining(paymentoutcome);
 });
 
 Then (/^I confirm the payment has failed with the reason (.*)$/, async (failedpaymentoutcome) => {
     await browser.saveScreenshot('./screenshots/screenshotfail.png');
-    await PaymentProcessingPage.failedPaymentResult.waitForDisplayed({timeout: 15000});
+    await PaymentProcessingPage.failedPaymentResult.waitUntil(async function () {
+        return (await this.getText()) === failedpaymentoutcome
+    }, {
+        timeout: 120000
+    });
     await browser.saveScreenshot('./screenshots/screenshotfailafter15secs.png');
     await expect(PaymentProcessingPage.failedPaymentResult).toBeExisting();
     await expect(PaymentProcessingPage.failedPaymentResult).toHaveTextContaining(failedpaymentoutcome);
